bb2e490112d8426bd4abe780c1c24d45e92b43cf
Hamdhanl54 <hlaiq9891@gmail.com>
2024-04-25 23:59:39 -0400

    j

--- Game_background/test2.py
+++ Game_background/test2.py
@@ -9,6 +9,38 @@ FPS = 1
 
 
 # ---------- Class ----------
+class Button:
+    def __init__(self, x, y, image, scale):
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.clicked = False
+    
+    def draw(self):
+        action = False
+
+        # Get mouse position
+        pos = pygame.mouse.get_pos()
+        
+        #cehck mouse over and clicked conditions
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+        return action
+
+play_button = Button(50, 200, play_img, 0.2)
+quit_button = Button(200, 200, quit_img, 0.2)
+
+
 class Game:
     def __init__(self):
         pygame.init()
