71becd116aeda4ef0e892856cf3e34c42ff4aa16
Hamdhanl54 <hlaiq9891@gmail.com>
2024-05-03 08:42:42 -0400

    h

--- Game_background/start.py
+++ Game_background/start.py
@@ -6,15 +6,33 @@ SCREEN_WIDTH = 750
 SCREEN_HEIGHT = 900
 
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Menu")
+pygame.display.set_caption("Monkey CLimb Menu")
 
-BACKGROUND = pygame.image.load('/Users/laiq/Desktop/Computer Science/Studio Code/03_Github/ICS4u/Images/Monkey_Climb.jpeg')
-
+# ------------------------------------------------------ IMAGES ------------------------------------------------------
+BACKGROUND_IMG = pygame.image.load('/Users/laiq/Desktop/Computer Science/Studio Code/03_Github/ICS4u/Images/Monkey_Climb.jpeg')
 BACKGROUND_SCALE = (750, 900)
-BACKGROUND = pygame.transform.scale(BACKGROUND, BACKGROUND_SCALE)
+BACKGROUND_IMG = pygame.transform.scale(BACKGROUND_IMG, BACKGROUND_SCALE)
 
 PLAY_BUTTON_IMG = pygame.image.load("/Users/laiq/Desktop/Computer Science/Studio Code/03_Github/ICS4u/Images/Start_Button.jpeg")
+PLAY_BUTTON_SCALE = (200, 100)
+PLAY_BUTTON_IMG = pygame.transform.scale(PLAY_BUTTON_IMG, PLAY_BUTTON_SCALE)
+
+OBJ_BUTTON_IMG = pygame.image.load("/Users/laiq/Desktop/Computer Science/Studio Code/03_Github/ICS4u/Images/Objective_Button.jpeg")
+OBJ_BUTTON_SCALE = (200, 100)
+OBJ_BUTTON_IMG = pygame.transform.scale(OBJ_BUTTON_IMG, OBJ_BUTTON_SCALE)
+
 EXIT_BUTTON_IMG = pygame.image.load("/Users/laiq/Desktop/Computer Science/Studio Code/03_Github/ICS4u/Images/Exit_Button.jpeg")
+EXIT_BUTTON_SCALE = (200, 100)
+EXIT_BUTTON_IMG = pygame.transform.scale(EXIT_BUTTON_IMG, EXIT_BUTTON_SCALE)
+
+BACK_BUTTON_IMG = pygame.image.load()
+BACK_BUTTON_SCALE = (200, 100)
+BACK_BUTTON_IMG = pygame.transform.scale(BACK_BUTTON_IMG, BACK_BUTTON_SCALE)
+
+OBJ_IMG = pygame.image.load("/Users/laiq/Desktop/Computer Science/Studio Code/03_Github/ICS4u/Images/Objective.jpg")
+OBJ_IMG_SCALE = (750, 900)
+OBJ_IMG = pygame.transform.scale(OBJ_IMG, OBJ_IMG_SCALE)
+
 
 def get_font(size): # Returns Press-Start-2P in the desired size
     return pygame.font.Font("/Users/laiq/Desktop/Computer Science/Studio Code/03_Github/ICS4u/font.ttf", size)
@@ -49,11 +67,7 @@ def options():
     while True:
         OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
 
-        SCREEN.fill("white")
-
-        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
-        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
-        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
+        SCREEN.blit(OBJ_IMG, (0, 0))
 
         OPTIONS_BACK = Button(image=None, pos=(640, 460), 
                             text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
@@ -73,20 +87,20 @@ def options():
 
 def main_menu():
     while True:
-        SCREEN.blit(BACKGROUND, (0,0))
+        SCREEN.blit(BACKGROUND_IMG, (0,0))
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
-        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+        MENU_TEXT = get_font(100).render("MONKEY CLIMB", True, "#000000")
+        MENU_RECT = MENU_TEXT.get_rect(center=(375, 100))
 
-        PLAY_BUTTON = Button(image=PLAY_BUTTON_IMG, pos=(120, 700), 
+        PLAY_BUTTON = Button(image=PLAY_BUTTON_IMG, pos=(120, 840), 
                             text_input="", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
         
-        RULES_BUTTON = Button(image=pygame.image.load("/Users/laiq/Desktop/Computer Science/Studio Code/03_Github/ICS4u/Images/Back_Monkey.jpeg"), pos=(640, 400), 
-                            text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        RULES_BUTTON = Button(image=OBJ_BUTTON_IMG, pos=(375, 840), 
+                            text_input="", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
         
-        QUIT_BUTTON = Button(image=EXIT_BUTTON_IMG, pos=(640, 550), 
+        QUIT_BUTTON = Button(image=EXIT_BUTTON_IMG, pos=(630, 840), 
                             text_input="", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
 
         SCREEN.blit(MENU_TEXT, MENU_RECT)
@@ -102,7 +116,7 @@ def main_menu():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
                     play()
-                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                if RULES_BUTTON.checkForInput(MENU_MOUSE_POS):
                     options()
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
                     pygame.quit()
