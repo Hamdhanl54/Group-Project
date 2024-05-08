abaa60ed31989f4ef4e7bb0d92d6e0a0a8f017d1
Hamdhanl54 <hlaiq9891@gmail.com>
2024-05-08 09:26:57 -0400

    h

--- Game_background/Monkey_Climb.py
+++ Game_background/Monkey_Climb.py
@@ -204,11 +204,11 @@ class Player():
 		self.index = 0
 		self.counter = 0
 		for num in range (1, 5):
-			img_right = pygame.image.load(f'Images/Monkey/Right&Left/run_img{num}.png')
-			img_right = pygame.transform.scale(img_right, (60, 60))
-			img_left = pygame.transform.flip(img_right, True, False)
-			self.images_right.append(img_right)
+			img_left = pygame.image.load(f'Images/Monkey/Right&Left/run_img{num}.png')
+			img_left = pygame.transform.scale(img_left, (60, 60))
+			img_right = pygame.transform.flip(img_left, True, False)
 			self.images_left.append(img_left)
+			self.images_right.append(img_right)
 		self.image = self.images_right[self.index]
 		
 		self.rect = self.image.get_rect()
